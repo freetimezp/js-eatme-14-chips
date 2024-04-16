e5e496b787852398bc3990e5526996ebe796ff39
freetimezp <kraftukrnet@ukr.net>
2024-04-16 20:12:53 +0300

    03. js-eatme-14-chips. Home section. Shadow header on scroll.

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,14 @@ const linkAction = () => {
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+//change shadow header
+const shadowHeader = () => {
+    const header = document.getElementById('header');
+    this.scrollY >= 50
+        ? header.classList.add('shadow-header')
+        : header.classList.remove('shadow-header');
+};
+
+window.addEventListener('scroll', shadowHeader);
