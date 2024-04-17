807760e1fd0fa26ffaced0304734cacb75f3b7c8
freetimezp <kraftukrnet@ukr.net>
2024-04-17 19:24:59 +0300

    11. js-eatme-14-chips. ScrollReveal animation.

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,3 +76,37 @@ const scrollActive = () => {
 }
 
 window.addEventListener('scroll', scrollActive);
+
+
+//animation
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 300,
+    reset: true
+});
+
+sr.reveal(`.home__data, favorites__container, .footer__container`);
+sr.reveal(`.home__circle`, { delay: 400, scale: 0.3 });
+sr.reveal(`.home__subcircle`, { delay: 500, scale: 0.2 });
+sr.reveal(`.home__img`, { delay: 600, scale: 0.1 });
+sr.reveal(`.home__chips-1, .home__chips-2, .home__chips-3`, { delay: 1000, interval: 100 });
+sr.reveal(`.home__leaf`, { delay: 1200 });
+sr.reveal(`.home__tomato-1`, { delay: 1500 });
+sr.reveal(`.home__tomato-2`, { delay: 1700 });
+sr.reveal(`.care__img, .contact__img`, { origin: 'left' });
+sr.reveal(`.care__list, .contact__data`, { origin: 'right' });
+sr.reveal(`.banner__item, .products__card`, { interval: 100 });
+
+
+
+
+
+
+
+
+
+
+
+
